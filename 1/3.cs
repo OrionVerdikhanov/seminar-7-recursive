c7e0c1a4c34dd3b80e3474dea01fcbaa726fc83c
Feitulla <113803928+OrionVerdikhanov@users.noreply.github.com>
2024-02-10 13:04:31 +0300

    Update 3.cs

--- 1/3.cs
+++ 1/3.cs
@@ -1,1 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Произвольный массив чисел
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        PrintArrayReverse(array, array.Length);
+    }
+
+    static void PrintArrayReverse(int[] arr, int length)
+    {
+        if (length > 0)
+        {
+            Console.WriteLine(arr[length - 1]);
+            PrintArrayReverse(arr, length - 1);
+        }
+    }
+}
 
